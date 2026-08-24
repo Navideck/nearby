55860a741f70baf0379da294fc50b389061485d7
Rohit Sangwan <rohitsangwan647@gmail.com>
2026-08-24 19:18:42 +0530

    Cleanup package

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import bonsoir_darwin
-import nearby
 import universal_ble
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
-  NearbyPlugin.register(with: registry.registrar(forPlugin: "NearbyPlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
 }
